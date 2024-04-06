0cded88acc6f1f4ec702bd19186e1b21387443f7
Vittorio D'Alfonso <vittoriodalfonso07@gmail.com>
2024-04-06 19:05:36 +0200

    Update modal title in create-modal.tsx

--- apps/mobile/app/(protected)/(home)/create-modal.tsx
+++ apps/mobile/app/(protected)/(home)/create-modal.tsx
@@ -5,7 +5,7 @@ import { H1, Muted } from "@/components/ui/typography";
 export default function Modal() {
 	return (
 		<View className="flex flex-1 items-center justify-center bg-background p-4 gap-y-4">
-			<H1 className="text-center">Modal</H1>
+			<H1 className="text-center">Create Modal</H1>
 			<Muted className="text-center">This the Create Modal...</Muted>
 		</View>
 	);

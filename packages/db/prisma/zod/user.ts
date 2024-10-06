d31eeff899e345b6eac7d37d9cff1283c40b841f
Vittorio D'Alfonso <vittoriodalfonso07@gmail.com>
2024-10-06 13:10:40 +0200

    🚀 Update to expo@51

--- packages/db/prisma/zod/user.ts
+++ packages/db/prisma/zod/user.ts
@@ -6,6 +6,8 @@ export const userSchema = z.object({
   phone: z.string().nullish(),
   firstName: z.string().nullish(),
   lastName: z.string().nullish(),
+  notifications: z.boolean(),
+  notificationToken: z.string().nullish(),
   createdAt: z.date(),
   updatedAt: z.date(),
 })
